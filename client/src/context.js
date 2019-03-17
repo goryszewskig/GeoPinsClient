fc994e91962cdf71a7c5d1dca093e890ef94117b
goryszewskig <greg.szewski@gmail.com>
2019-03-17 09:56:01 +0100

    deploy ready

---
+++ client/src/context.js
@@ -0,0 +1,15 @@
+import { createContext } from 'react';
+
+
+const Context = createContext({
+    currentUser : null,
+    isAuth: false,
+    draft: null,
+    pins: [],
+    currentPin: null
+})
+
+
+export default Context;
+
+
